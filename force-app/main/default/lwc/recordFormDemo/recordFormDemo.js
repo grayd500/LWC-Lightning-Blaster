1b40ceaa708a522a5c64120bc9cf0e635e85497d
David <grayd500@gmail.com>
2024-02-20 13:01:18 -0500

    Update with edit form on record page

--- force-app/main/default/lwc/recordFormDemo/recordFormDemo.js
+++ force-app/main/default/lwc/recordFormDemo/recordFormDemo.js
@@ -1,4 +1,4 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
 import ACCOUNT_OBJECT from '@salesforce/schema/Account'
@@ -7,6 +7,8 @@ import ANNUAL_REVENUE_FIELD from '@salesforce/schema/Account.AnnualRevenue'
 import TYPE_FIELD from '@salesforce/schema/Account.Type'
 import INDUSTRY_FIELD from '@salesforce/schema/Account.Industry'
 export default class RecordFormDemo extends LightningElement {
+    @api recordId
+    @api objectApiName
     objectName = ACCOUNT_OBJECT
     fieldList = [NAME_FIELD, ANNUAL_REVENUE_FIELD, TYPE_FIELD, INDUSTRY_FIELD]
 
