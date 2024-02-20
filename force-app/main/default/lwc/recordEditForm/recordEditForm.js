011e1a6b4a0fe0458ec575ee32f21712454b6120
David <grayd500@gmail.com>
2024-02-20 16:32:23 -0500

    Update with field reset on record edit form in Base Components

--- force-app/main/default/lwc/recordEditForm/recordEditForm.js
+++ force-app/main/default/lwc/recordEditForm/recordEditForm.js
@@ -14,4 +14,12 @@ export default class RecordEditForm extends LightningElement {
         phoneField:PHONE_FIELD,
         emailField:EMAIL_FIELD
     }
+    handleReset(){
+        const inputFields = this.template.querySelectorAll('lightning-input-field')
+        if(inputFields){
+            Array.from(inputFields).forEach(field=>{
+               field.reset() 
+            })
+        }
+    }
 }
